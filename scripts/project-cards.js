96963d9119e04c9a4e84fce2b58587629349d1e8
lordtootleberry <96302605+lordtootleberry@users.noreply.github.com>
2023-01-22 20:10:41 +0000

    formatting

--- scripts/project-cards.js
+++ scripts/project-cards.js
@@ -1,34 +1,34 @@
 console.log("pc");
 
-function doTheThing(){
+function doTheThing() {
 
-console.log("thing done");
+  console.log("thing done");
 };
 
 
 function createProjectCards() {
   let projectCards = [
-      {
-        title: 'Project 1',
-        description: 'This is the first project.',
-        image: 'https://placekitten.com/200/200'
-      },
-      {
-        title: 'Project 2',
-        description: 'This is the second project.',
-        image: 'https://placekitten.com/200/200'
-      },
-      {
-        title: 'Project 3',
-        description: 'This is the third project.',
-        image: 'https://placekitten.com/200/200'
-      },
-      {
-        title: 'Project 4',
-        description: 'This is the fourth project.',
-        image: 'https://placekitten.com/200/200'
-      }
-    ];
-  }
+    {
+      title: 'Project 1',
+      description: 'This is the first project.',
+      image: 'https://placekitten.com/200/200'
+    },
+    {
+      title: 'Project 2',
+      description: 'This is the second project.',
+      image: 'https://placekitten.com/200/200'
+    },
+    {
+      title: 'Project 3',
+      description: 'This is the third project.',
+      image: 'https://placekitten.com/200/200'
+    },
+    {
+      title: 'Project 4',
+      description: 'This is the fourth project.',
+      image: 'https://placekitten.com/200/200'
+    }
+  ];
+}
 
 // function drawProjectCards(){}

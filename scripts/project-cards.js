48f866a8c4c64d7b941e2dbfd7de96453b8327a0
lordtootleberry <96302605+lordtootleberry@users.noreply.github.com>
2023-01-22 19:52:43 +0000

    loaded p5js script

---
+++ scripts/project-cards.js
@@ -0,0 +1,38 @@
+console.log("pc");
+
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw(){
+  background(0);
+  fill(255, 0, 0);
+  ellipse(200,200,200,200);
+}
+
+// function createProjectCards() {
+//   let projectCards = [
+//       {
+//         title: 'Project 1',
+//         description: 'This is the first project.',
+//         image: 'https://placekitten.com/200/200'
+//       },
+//       {
+//         title: 'Project 2',
+//         description: 'This is the second project.',
+//         image: 'https://placekitten.com/200/200'
+//       },
+//       {
+//         title: 'Project 3',
+//         description: 'This is the third project.',
+//         image: 'https://placekitten.com/200/200'
+//       },
+//       {
+//         title: 'Project 4',
+//         description: 'This is the fourth project.',
+//         image: 'https://placekitten.com/200/200'
+//       }
+//     ];
+//   }
+
+// function drawProjectCards(){}

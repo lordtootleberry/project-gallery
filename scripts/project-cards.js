67f6c9b2ee9928cbd6f6a3f0ef8374236f17c84a
lordtootleberry <96302605+lordtootleberry@users.noreply.github.com>
2023-02-04 17:12:57 +0000

    add function to generate project cards

--- scripts/project-cards.js
+++ scripts/project-cards.js
@@ -1,47 +1,61 @@
-console.log("pc");
 
-function addElementToPage() {
+function generateProjectCards() {
 
-  console.log("thing done");
-
-  const nodeToAdd = document.createElement("div");
-// not adding to end of body, but to end of projects div
-  // document.body.appendChild(nodeToAdd);
-
-  const textNode = document.createTextNode("wowzers "+ Math.random().toFixed(1));
-
-  nodeToAdd.appendChild(textNode);
-
-  const projectEl = document.getElementById("projects");
-  
-  projectEl.appendChild(nodeToAdd);
-
-};
-
-
-function createProjectCards() {
   let projectCards = [
     {
       title: 'Project 1',
       description: 'This is the first project.',
-      image: 'https://placekitten.com/200/200'
+      image: "/images/dog-1"
     },
     {
       title: 'Project 2',
       description: 'This is the second project.',
-      image: 'https://placekitten.com/200/200'
+      image: "/images/dog-2"
     },
     {
       title: 'Project 3',
       description: 'This is the third project.',
-      image: 'https://placekitten.com/200/200'
+      image: "/images/dog-3"
     },
     {
       title: 'Project 4',
       description: 'This is the fourth project.',
-      image: 'https://placekitten.com/200/200'
+      image: "/images/dog-4"
     }
   ];
+
+
+   //find the projectSection and store it in a variable called projectCardSection
+   const projectCardSection = document.getElementById("projectSection");
+   
+  //loop through the cards to generate the HTML
+  for (let card of projectCards) {
+
+    //create the card div
+    const cardDiv = document.createElement("div");
+    //create a title node
+    const titleNode = document.createTextNode(card.title);
+    //append the title node to the card div
+    cardDiv.appendChild(titleNode);
+
+    //create a description node
+    const descriptionNode = document.createTextNode(card.description);
+    //append the description node to the card div
+    cardDiv.appendChild(descriptionNode);
+
+    //create an image element
+    const imageElement = document.createElement("img");
+    //set the src attribute of the image element
+    imageElement.setAttribute("src", card.image);
+    //append the image element to the card div
+    cardDiv.appendChild(imageElement);
+
+    console.log(projectCardSection);
+
+    //append the card div to the project card section
+    projectCardSection.appendChild(cardDiv);
+  }
 }
 
-// function drawProjectCards(){}
+//call the function to generate the cards
+generateProjectCards();
